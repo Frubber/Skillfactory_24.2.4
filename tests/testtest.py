dea937a5dd192eeb717c1e57b181aa01176b42ad
Frubber <122484309+Frubber@users.noreply.github.com>
2023-05-18 19:57:15 +0300

    Add files via upload

---
+++ tests/testtest.py
@@ -0,0 +1,24 @@
+import pytest
+
+from app.calc import Calculator
+
+class TestCalc:
+    def setup(self):
+        self.calc = Calculator
+
+
+    def test_multiply_success(self):
+        assert self.calc.multiply(self, 2, 2) == 4
+
+    def test_zero_division(self):
+        assert self.calc.division(self, 4, 2) == 2
+
+    def test_subtraction_success(self):
+        assert self.calc.subtraction(self, 2, 1) == 1
+
+    def test_adding_success(self):
+        assert self.calc.adding(self, 1, 1) == 2
+
+
+    def teardown(self):
+        print('Выполнение метода Teardown')
